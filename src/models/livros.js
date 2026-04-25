a97d99f952dff4e76a4d1d6b0ab7438b29b4b4dc
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-25 17:45:52 -0400

    NodeJs_Api

---
+++ src/models/livros.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { required } from "nodemon/lib/config";
+
+const livroSchema = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    titulo: { type: String, required: true },
+    editora: { type: String },
+    preco: { type: Number },
+    paginas: { type: Number },
+  },
+  { versionKey: false },
+);
+
+const livro = mongoose.model("livro", livroSchema);
+
+export default livro;
