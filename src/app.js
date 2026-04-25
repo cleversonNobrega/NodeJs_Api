243f38293c82903f0f3566163226672026b1c19c
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-25 11:04:59 -0400

    NodeJs_Api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const livros = [
   {
@@ -13,6 +14,12 @@ const livros = [
   },
 ];
 
+function buscaLivro(id) {
+  return livros.findIndex((livros) => {
+    return livros.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
@@ -21,8 +28,14 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
 
 export default app;
