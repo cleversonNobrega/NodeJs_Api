407d135d8fba0ceb8244b018ee3b36c9784643ef
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-24 22:55:17 -0400

    NodeJs_Api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 
 const rotas = {
   "/": "Curso de Express Api",
+  "/Livros": "Entrei na rota livros",
+  "/autores": "Entrei na rota autores",
 };
 
 const server = http.createServer((req, res) => {
