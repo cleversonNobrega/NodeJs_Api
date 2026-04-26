7ab8ec570ab3c3d8611f0a2228c3245f62341d85
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-26 13:24:35 -0400

    NodeJs_Api

--- src/middllewares/manipuladorDeErros.js
+++ src/middllewares/manipuladorDeErros.js
@@ -5,6 +5,16 @@ function manipuladorDeErros(erro, req, res, next) {
     res
       .status(400)
       .send({ message: "Um ou mais dados fornecidos estão incorretos." });
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(erro.errors)
+      .map((erro) => erro.message)
+      .join(";");
+
+    res
+      .status(400)
+      .send({
+        message: `Os seguintes erros foram encontrados: ${mensagensErro}`,
+      });
   } else {
     res.status(500).send({ message: "Erro interno de servidor." });
   }
