69ce1fe7ed7c525863b72803798bc67364841b78
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-25 22:07:28 -0400

    NodeJs_Api

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,4 +1,5 @@
 import livro from "../models/livros.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
   static async listarLivros(req, res) {
@@ -25,9 +26,17 @@ class LivroController {
   }
 
   static async cadastrarLivro(req, res) {
+    const novoLivro = req.body;
     try {
-      const novoLivro = await livro.create(req.body);
-      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = {
+        ...novoLivro,
+        autor: { ...autorEncontrado._doc },
+      };
+      const livroCriado = await livro.create(livroCompleto);
+      res
+        .status(201)
+        .json({ message: "criado com sucesso", livro: livroCriado });
     } catch (erro) {
       res
         .status(500)
@@ -54,6 +63,16 @@ class LivroController {
       res.status(200).json({ message: "livro excluído com sucesso" });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    }
+  }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na busca` });
     }
   }
 }
